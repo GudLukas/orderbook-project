1c7b49f6289b5150e816c268cdce55d8c7b5fe9b
devOrdinai <gudeliuberniokas@gmail.com>
2025-07-03 12:31:10 +0100

    simple setup with some text + pytest for CI

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, Orderbook!"
+def index():
+    return "<h1>Hello, Orderbook!</h1>" \
+    "<p>Welcome to the Orderbook application.</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
